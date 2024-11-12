fd4bc11dc99c87d1535467399a9d4fd91b6dedcd
Jetro-Natan <jetr08@gmail.com>
2024-11-12 20:25:30 -0300

    Atividade dia 1

---
+++ atividadedia1.js
@@ -0,0 +1,25 @@
+
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+if (numeroUm == stringUm && numeroUm !== stringUm) {
+  alert('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes');
+} else {
+  alert('As variáveis numeroUm e stringUm não têm o mesmo valor');
+}
+
+if (numeroTrinta === stringTrinta) {
+  alert('As variáveis numeroTrinta e stringTrinta têm o mesmo valor e mesmo tipo');
+} else {
+  alert('As variáveis numeroTrinta e stringTrinta não têm o mesmo tipo');
+}
+
+if (numeroDez == stringDez && numeroDez !== stringDez) {
+  alert('As variáveis numeroDez e stringDez têm o mesmo valor, mas tipos diferentes');
+} else {
+  alert('As variáveis numeroDez e stringDez não têm o mesmo valor');
+}
