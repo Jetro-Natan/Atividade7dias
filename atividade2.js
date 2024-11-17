6a1435c92d7944c42fbe4e07126b1cd3335a1a73
Jetro-Natan <jetr08@gmail.com>
2024-11-17 16:59:19 -0300

    Update atividade2.js

--- atividade2.js
+++ atividade2.js
@@ -1,12 +1,14 @@
-
-alert("Olá, bem-vindo ao programa!");
-
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
 const linguagem = prompt("Qual linguagem de programação você está estudando?");
-const resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
 
-if (resposta == 1) {
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+
+const gostaDeEstudar = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+if (gostaDeEstudar == "1") {
     alert("Muito bom! Continue estudando e você terá muito sucesso.");
-} else if (resposta == 2) {
+} else if (gostaDeEstudar == "2") {
     alert("Ahh que pena... Já tentou aprender outras linguagens?");
 } else {
     alert("Resposta inválida. Por favor, responda apenas com 1 ou 2.");
